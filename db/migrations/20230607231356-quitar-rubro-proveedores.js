c2e6f6ef370b6eb1b7e615e42563e6d1a0e90c62
gutierrezraul <raul.gutierrez@estudiantes.unahur.edu.ar>
2023-06-08 16:01:29 -0300

    Req.3 integracion

--- db/migrations/20230607231356-quitar-rubro-proveedores.js
+++ db/migrations/20230607231356-quitar-rubro-proveedores.js
@@ -1,11 +1,11 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  async up(queryInterface, Sequelize) {
     await queryInterface.removeColumn('Proveedores', 'rubro');
   },
 
-  down: async (queryInterface, Sequelize) => {
+  async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Proveedores');
   },
 };
