04ebbb7fc3ce5bfb7ae02aeb083cf136727a6132
frodriguezok <frodriguezok@gmail.com>
2023-10-09 21:08:11 -0300

    Cambio en tipo de dato de fecha de compras

--- db/migrations/0008-create-tabla-compras.js
+++ db/migrations/0008-create-tabla-compras.js
@@ -10,7 +10,7 @@ module.exports = {
         type: Sequelize.INTEGER,
       },
       fecha: {
-        type: Sequelize.DATE,
+        type: Sequelize.DATEONLY,
       },
       numeroCompra: {
         type: Sequelize.INTEGER,
